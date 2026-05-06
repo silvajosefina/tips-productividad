808ee218ab99f721491527164573b18e094eeb2e
Josefina <silvajosefinad02@gmail.com>
2026-05-06 01:33:38 -0300

    guardar votos en localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,51 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [tips, setTips] = useState([
-    {
-      id: 1,
-      tip: 'Establece metas claras y alcanzables para cada día.',
-      votos: 0,
-    },
-    {
-      id: 2,
-      tip: 'Prioriza tus tareas utilizando la matriz de Eisenhower.',
-      votos: 0,
-    },
-    {
-      id: 3,
-      tip: 'Utiliza la técnica Pomodoro para mantener la concentración.',
-      votos: 0,
-    },
-    {
-      id: 4,
-      tip: 'Elimina distracciones, como las redes sociales, durante el trabajo.',
-      votos: 0,
-    },
-    {
-      id: 5,
-      tip: 'Toma descansos regulares para recargar energías.',
-      votos: 0,
-    },
-    {
-      id: 6,
-      tip: 'Organiza tu espacio de trabajo para mejorar la eficiencia.',
-      votos: 0,
-    },
-    {
-      id: 7,
-      tip: 'Utiliza herramientas de gestión de tareas como Trello o Asana.',
-      votos: 0,
-    },
-    {
-      id: 8,
-      tip: 'Aprende a delegar tareas cuando sea posible.',
-      votos: 0,
-    }
-  ]);
+  const datosGuardados = localStorage.getItem("tips");
+
+  const [tips, setTips] = useState(
+    datosGuardados ? JSON.parse(datosGuardados) : [
+      {
+        id: 1,
+        tip: 'Establece metas claras y alcanzables para cada día.',
+        votos: 0,
+      },
+      {
+        id: 2,
+        tip: 'Prioriza tus tareas utilizando la matriz de Eisenhower.',
+        votos: 0,
+      },
+      {
+        id: 3,
+        tip: 'Utiliza la técnica Pomodoro para mantener la concentración.',
+        votos: 0,
+      },
+      {
+        id: 4,
+        tip: 'Elimina distracciones, como las redes sociales, durante el trabajo.',
+        votos: 0,
+      },
+      {
+        id: 5,
+        tip: 'Toma descansos regulares para recargar energías.',
+        votos: 0,
+      },
+      {
+        id: 6,
+        tip: 'Organiza tu espacio de trabajo para mejorar la eficiencia.',
+        votos: 0,
+      },
+      {
+        id: 7,
+        tip: 'Utiliza herramientas de gestión de tareas como Trello o Asana.',
+        votos: 0,
+      },
+      {
+        id: 8,
+        tip: 'Aprende a delegar tareas cuando sea posible.',
+        votos: 0,
+      }
+    ]);
 
   const [tipActual, setTipActual] = useState(0);
 
@@ -63,11 +66,11 @@ function App() {
       votos: tipSeleccionado.votos + 1,
     };
     setTips(nuevosTips);
+    localStorage.setItem("tips", JSON.stringify(nuevosTips));
   };
 
   const reiniciarVotos = () => {
     const nuevosTips = [...tips];
-
     for (let i = 0; i < nuevosTips.length; i++) {
       nuevosTips[i] = {
         ...nuevosTips[i],
@@ -75,6 +78,7 @@ function App() {
       };
     }
     setTips(nuevosTips);
+    localStorage.setItem("tips", JSON.stringify(nuevosTips));
   };
 
   return (
